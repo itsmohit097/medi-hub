f6740d1d542ac3512760e987f00421ebae594a9b
khushi123go <tiwarikhushi907@gmail.com>
2024-05-11 08:20:42 +0530

    made navbar responsive and sticky

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,28 +1,57 @@
-import { NavLink } from "react-router-dom";
-import { useState, useEffect } from "react";
+import { Link, NavLink } from "react-router-dom";
+import { useState, useEffect, useRef } from "react";
 
 import logo from "/logo.svg";
 import { FaDiscord } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
 import { FaLinkedinIn } from "react-icons/fa";
+import { BiMenu } from 'react-icons/bi'
+
+const navLinks = [
+  {
+    path: '/',
+    display: 'Home'
+  },
+  {
+    path: '/alldoctors',
+    display: 'All Doctors'
+  },
+  {
+    path: '/appointment',
+    display: 'Appointment'
+  },
+  {
+    path: '/aboutus',
+    display: 'About Us'
+  },
+  {
+    path: '/login',
+    display: 'Login'
+  }
+]
 
 function Navbar() {
   const [scrolled, setScrolled] = useState(false);
+  const menuRef = useRef(null)
 
   const handleScroll = () => {
     const offset = window.scrollY;
-    console.log(offset);
     if (offset > 200) {
-      setScrolled(false);
+      setScrolled(true); 
     } else {
-      setScrolled(false);
+      setScrolled(false); 
     }
   };
 
   // sticky navbar on scroll
   useEffect(() => {
     window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll); 
+    };
   }, []);
+
+  const toggleMenu = () => menuRef.current.classList.toggle('show_menu')
 
   return (
     <div
@@ -31,79 +60,40 @@ function Navbar() {
     >
       <div className="max-w-7xl mx-auto flex items-center justify-between lg:px-6  px-3 py-3">
         {/* logo */}
-        <div className="flex items-center gap-3">
-          <img src={logo} alt="" className="w-8 " />
-          <h1 className="font-medium text-2xl text-text">MediHub</h1>
-        </div>
+        <Link to='/'>
+          <div className="flex items-center gap-3">
+            <img src={logo} alt="" className="w-8 " />
+            <h1 className="font-medium text-2xl text-text">MediHub</h1>
+          </div>
+        </Link>
 
-        {/* Nav Menus */}
-        <div className="hidden md:block">
-          <ul className="flex justify-between gap-8 items-center">
-            <li>
-              <NavLink
-                to={"/"}
-                className={({ isActive }) =>
-                  `text-sm font-semibold relative cursor-pointer before:block before:absolute before:bottom-[-4px] before:left-0 before:w-0 before:h-0.5 before:rounded-full before:bg-text before:transition-all before:delay-150 before:ease-in-out hover:before:w-full hover:text-text ${isActive ? "text-text" : "text-text/70"
-                  }`
-                }
-              >
-                Home
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
-                to={"/alldoctors"}
-                className={({ isActive }) =>
-                  `text-sm font-semibold relative cursor-pointer before:block before:absolute before:bottom-[-4px] before:left-0 before:w-0 before:h-0.5 before:rounded-full before:bg-text before:transition-all before:delay-150 before:ease-in-out hover:before:w-full hover:text-text ${isActive ? "text-text" : "text-text/70"
-                  }`
-                }
-              >
-                All Doctors
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
-                to={"/appointment"}
-                className={({ isActive }) =>
-                  `text-sm font-semibold relative cursor-pointer before:block before:absolute before:bottom-[-4px] before:left-0 before:w-0 before:h-0.5 before:rounded-full before:bg-text before:transition-all before:delay-150 before:ease-in-out hover:before:w-full hover:text-text ${isActive ? "text-text" : "text-text/70"
-                  }`
-                }
-              >
-                Appointment
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
-                to={"/aboutus"}
-                className={({ isActive }) =>
-                  `text-sm font-semibold relative cursor-pointer before:block before:absolute before:bottom-[-4px] before:left-0 before:w-0 before:h-0.5 before:rounded-full before:bg-text before:transition-all before:delay-150 before:ease-in-out hover:before:w-full hover:text-text ${isActive ? "text-text" : "text-text/70"
-                  }`
-                }
-              >
-                About Us
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
-                to={"/login"}
-                className={({ isActive }) =>
-                  `text-sm font-semibold relative cursor-pointer before:block before:absolute before:bottom-[-4px] before:left-0 before:w-0 before:h-0.5 before:rounded-full before:bg-text before:transition-all before:delay-150 before:ease-in-out hover:before:w-full hover:text-text ${isActive ? "text-text" : "text-text/70"
-                  }`
-                }
-              >
-                Login
-              </NavLink>
-            </li>
-            <li className="hover:scale-105">
-              <NavLink
-                to={"/signup"}
-                className="text-sm text-theme bg-text font-semibold relative cursor-pointer border rounded-3xl px-5 py-2 "
-              >
-                Sign Up
-              </NavLink>
-            </li>
+        {/* nav menu */}
+        <div className={`navigation md:flex ${scrolled ? 'hidden' : 'block'}`} ref={menuRef} onClick={toggleMenu}>
+          <ul className="menu flex items-center gap-[2.7rem]">
+            {navLinks.map((link, index) => (
+              <li key={index}>
+                <NavLink
+                  to={link.path}
+                  className={({ isActive }) =>
+                    `text-sm font-semibold relative cursor-pointer before:block before:absolute before:bottom-[-4px] before:left-0 before:w-0 before:h-0.5 before:rounded-full before:bg-text before:transition-all before:delay-150 before:ease-in-out hover:before:w-full hover:text-text ${isActive ? "text-text" : "text-text/70"
+                    }`
+                  }
+                >
+                  {link.display}
+                </NavLink>
+              </li>
+
+            ))}
           </ul>
+
         </div>
+        <NavLink
+          to={"/signup"}
+          className="text-sm text-theme bg-text font-semibold relative cursor-pointer border rounded-3xl px-5 py-2 ml-16 md:ml-0 "
+        >
+          Sign Up
+        </NavLink>
+
 
         {/* social icons */}
         <div className="flex gap-4 items-center">
@@ -121,7 +111,7 @@ function Navbar() {
             }
             target="_blank"
           >
-            <FaGithub className="text-text size-5 hover:scale-110 " />
+            <FaGithub className="text-text size-5 hidden md:block hover:scale-110" />
           </NavLink>
           <NavLink
             to={"https://www.linkedin.com/in/itsmohit097/"}
@@ -130,6 +120,11 @@ function Navbar() {
             <FaLinkedinIn className="text-text size-5 hidden md:block hover:scale-110" />
           </NavLink>
         </div>
+
+        {/* hamburger menu */}
+        <span className='md:hidden' onClick={toggleMenu}>
+          <BiMenu className='w-6 h-6 cursor-pointer text-white' />
+        </span>
       </div>
     </div>
   );
