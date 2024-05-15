04fccc8f9dba1df1d6a25ce958b3b7501ec9c9ed
Mohit Kumar <125040386+mohit-0007@users.noreply.github.com>
2024-05-15 10:30:04 +0530

    connect contact us with backend api

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import axios from "axios";
+import { axios } from "../import-export/ImportExport";
 import { NavLink } from "react-router-dom";
 import { useState } from "react";
 
