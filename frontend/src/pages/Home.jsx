596e358f3edbe11de0b96a4c11ed2024682bb990
pspriyanshu601 <priyanshusingh6014@gmail.com>
2024-05-29 20:45:41 +0530

    enhancement:added What is medihub section in home

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
-import { Contributors, Hero, WhyUs, } from "../import-export/ImportExport";
+import { Contributors, Hero, WhatIsMedihub, WhyUs, } from "../import-export/ImportExport";
 
 function Home() {
     return (
         <>
             <Hero />
             <WhyUs />
+            <WhatIsMedihub/>
             <Contributors />
         </>
     )
