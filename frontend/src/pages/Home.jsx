559b30efff0c29c9488207f2274f2b943bfa9532
Anshuman Tiwari <87803348+Ansh101112@users.noreply.github.com>
2024-06-02 06:50:29 +0000

    new sections added

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Contributors, Hero, WhatIsMedihub, WhyUs, Testamonial } from "../import-export/ImportExport";
+import Stats from '../components/Stats';
 
 function Home() {
     return (
         <>
             <Hero />
             <WhyUs />
+            <Stats></Stats>
             <WhatIsMedihub/>
             <Testamonial/>
             <Contributors />
