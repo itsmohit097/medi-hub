8cc317a037fc32ac0819955ee2e38aeda92da7b1
Mohit Kumar <125040386+mohit-0007@users.noreply.github.com>
2024-06-02 20:47:56 +0530

    fixes deployment error

--- frontend/src/import-export/ImportExport.js
+++ frontend/src/import-export/ImportExport.js
@@ -14,7 +14,7 @@ import MedicineCard from "../components/MedicineCard.jsx";
 import Bot from "../components/Bot.jsx";
 import FAQSection from "../components/Faq.jsx";
 import Testamonial from "../components/Testamonial.jsx";
-import OrderedProduct from "../components/OrderedProduct.jsx";
+import OrderedProduct from "../components/CartPage.jsx";
 
 
 // Pages
@@ -31,15 +31,16 @@ import AddtoCart from "../pages/AddtoCart.jsx";
 import Categoryitems from "../pages/Categoryitems.jsx";
 import SingleMedicine from "../pages/SingleMedicine.jsx";
 import Medicines from "../pages/Medicines.jsx";
+import CartPage from "../components/CartPage.jsx";
 
 import axios from "../axios/axios.jsx";
 
 
-export { 
-  axios, Navbar, Footer, Home, Hero, WhyUs, Contributors, Appointment, FAQSection, 
-  AppointmentLocation, Departments, AllDoctors, AboutUs, Login, Signup, ErrorPage, 
-  WhatIsMedihub, SkeletonLoading, TermsAndConditions, PrivacyPolicy, AddtoCart, 
-  Categoryitems, SingleMedicine, Medicines, GoToTop, Pagination, MedicineCard, 
-  Bot, Testamonial 
+export {
+  axios, Navbar, Footer, Home, Hero, WhyUs, Contributors, Appointment, FAQSection,
+  AppointmentLocation, Departments, AllDoctors, AboutUs, Login, Signup, ErrorPage,
+  WhatIsMedihub, SkeletonLoading, TermsAndConditions, PrivacyPolicy, AddtoCart,
+  Categoryitems, SingleMedicine, Medicines, GoToTop, Pagination, MedicineCard,
+  Bot, Testamonial, CartPage
 };
 
