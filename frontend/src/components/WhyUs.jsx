b208adfb59660ff939b0c31e76a9770426e0c9bd
RRJ <riyaranjanjha2006@gmail.com>
2024-06-04 12:54:38 +0530

    Services section

--- frontend/src/components/WhyUs.jsx
+++ frontend/src/components/WhyUs.jsx
@@ -7,75 +7,68 @@ import { FaRegBuilding } from "react-icons/fa";
 
 function WhyUs() {
   return (
-    <div className=" w-full md:py-24 py-12">
+    <div className=" w-full md:pb-10 md:pt-16 py-4">
       <div className="max-w-7xl mx-auto flex flex-col items-center h-full lg:px-6 lg:py-8 lg:justify-center px-0 py-3">
         <h1 className=" text-2xl md:text-4xl lg:text-5xl font-bold text-theme text-center">
-          Why Choose Us
+          Our Services
         </h1>
-        <p className="text-xs md:text-base md:max-w-2xl mt-6 text-center">
-          We understand that your health and well-being are of paramount
-          importance. Here are compelling reasons why you should choose us for
-          your healthcare needs
+        <p className="text-xs p-3 md:text-base md:max-w-2xl mt-6 text-center">
+          Skilline is one powerful online software suite that combines all the
+          tools needed to run a successful school or office.
         </p>
 
         {/* cards */}
-        <div className="grid grid-cols-1 lg:grid-rows-1 lg:grid-cols-3 gap-x-8 gap-y-4 md:gap-y-10 lg:py-8 mt-12 md:mt-14 lg:mt-20 px-3">
+        <div className="grid grid-cols-1 lg:grid-rows-1 lg:grid-cols-3 gap-x-8 gap-y-4 md:gap-y-6 lg:py-8  px-3">
           {/* Aid card  */}
-          <div className="flex flex-col items-start shadow-md my-4 rounded-lg md:px-4 hover:shadow-xl hover:border border-theme py-4 bg-pastel_blue">
+          <div className="flex flex-col items-start shadow-md my-4 rounded-lg md:px-4 hover:shadow-xl  border-theme py-4 bg-pastel_blue">
             <div className="flex flex-col justify-between items-start gap-y-4 px-3 md:px-0">
-              <div className="icon">
+              <div className="icon relative -top-[30%] left-[125px]">
                 <BiSolidFirstAid className="size-20 text-red-600 border border-red-600 rounded-full py-4" />
               </div>
 
-              <div className="text">
-                <h1 className="mb-2 text-md text-left font-semibold uppercase text-theme/80">
+              <div className="text -mt-[35px]">
+                <h1 className="mb-2 text-md text-center font-semibold uppercase text-theme/80">
                   Advanced Technology
                 </h1>
-                <p className="mt-0 text-sm text-left text-black text-">
-                  Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                  Commodi repellat numquam, dolore necessitatibus excepturi
-                  velit saepe consequatur magnam, unde officiis, hic ipsam nemo
-                  praesentium nam quo.
+                <p className="mt-0 text-sm text-center text-black ">
+                  Simple and secure control of your organization's financial and
+                  legal transactions. Send customized invoices and contracts.
                 </p>
               </div>
             </div>
           </div>
           {/* Doctors card */}
-          <div className="flex flex-col items-start shadow-md my-4 rounded-lg md:px-4 hover:shadow-xl hover:border border-theme py-4 bg-pastel_yellow">
+          <div className="flex flex-col items-start shadow-md my-4 rounded-lg md:px-4 hover:shadow-xl  border-theme py-4 bg-pastel_yellow">
             <div className="flex flex-col justify-between items-start gap-y-4 px-3 md:px-0">
-              <div className="icon">
+              <div className="icon relative -top-[30%] left-[125px]">
                 <FaUserDoctor className="size-20 text-green-600 border border-green-600 rounded-full py-4" />
               </div>
 
-              <div className="text">
-                <h1 className="mb-2 text-md text-left font-semibold uppercase text-theme/80">
+              <div className="text -mt-[35px]">
+                <h1 className="mb-2 text-md text-center font-semibold uppercase text-theme/80">
                   Certified Doctors
                 </h1>
-                <p className="mt-0 text-sm text-left text-black text-">
-                  Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                  Commodi repellat numquam, dolore necessitatibus excepturi
-                  velit saepe consequatur magnam, unde officiis, hic ipsam nemo
-                  praesentium nam quo.
+                <p className="mt-0 text-sm text-center text-black text-">
+                  Schedule and reserve classroom at one campus or multiple
+                  campuses. Keep detailed records of student's attendance.
                 </p>
               </div>
             </div>
           </div>
           {/* Infrastrucutre card */}
-          <div className="flex flex-col items-start shadow-md my-4 rounded-lg md:px-4 hover:shadow-xl hover:border border-theme py-4 bg-pastel_pink">
+          <div className="flex flex-col items-start shadow-md my-4 rounded-lg md:px-4 hover:shadow-xl border-theme py-4 bg-pastel_pink">
             <div className="flex flex-col justify-between items-start gap-y-4 px-3 md:px-0">
-              <div className="icon">
+              <div className="icon relative -top-[30%] left-[125px]">
                 <FaRegBuilding className="size-20 text-gray-900 border border-gray-900 rounded-full py-4" />
               </div>
 
-              <div className="text">
-                <h1 className="mb-2 text-md text-left font-semibold uppercase text-theme/80">
+              <div className="text  -mt-[35px]">
+                <h1 className="mb-2 text-md font-semibold uppercase text-center text-theme/80">
                   Best Infrastrucutre
                 </h1>
-                <p className="mt-0 text-sm text-left text-black text-">
-                  Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                  Commodi repellat numquam, dolore necessitatibus excepturi
-                  velit saepe consequatur magnam, unde officiis, hic ipsam nemo
-                  praesentium nam quo.
+                <p className="mt-0 text-sm text-center text-black text-">
+                  Automate and track emails to individuals or groups. Skilline's
+                  built-in system helps organize your organization.
                 </p>
               </div>
             </div>
